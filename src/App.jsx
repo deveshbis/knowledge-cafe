72c6e939a1bc1831f9c0095091ddbd103b550425
deveshbis <deveshbiswas.cse@gmail.com>
2024-03-13 11:00:22 +0600

    load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Header from './Components/Header/Header'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       
       <Header></Header>
+      <Blogs></Blogs>
       
       
     </>
